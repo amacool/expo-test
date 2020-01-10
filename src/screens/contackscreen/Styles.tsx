67dfae5a01641495f4f3e456574a18854746cbcc
truth86 <anatoly-sokolov@outlook.com>
2020-01-10 09:58:18 -0400

    Init Contact page

---
+++ src/screens/contackscreen/Styles.tsx
@@ -0,0 +1,83 @@
+import { ImageStyle, StyleSheet, TextStyle, ViewStyle } from "react-native";
+import Colors from "../../constants/Colors";
+
+interface CheckOutScreenStyles {
+  container: ViewStyle;
+  welcomeContainer: ViewStyle;
+  buttonContainer: ViewStyle;
+  formText: ViewStyle;
+  inValidForm: ViewStyle;
+  formSwitch: ViewStyle;
+  uploadBotton: ViewStyle;
+  problemBotton: ViewStyle;
+  createBotton: ViewStyle;
+  claimText: TextStyle;
+  problemText: TextStyle;
+}
+
+export const styles = StyleSheet.create<CheckOutScreenStyles>({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.mainBackground,
+  },
+  welcomeContainer: {
+    marginTop: 30,
+    alignItems: "center",
+    paddingHorizontal: 0,
+  },
+  buttonContainer: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+  formText: {
+    width: "100%",
+    backgroundColor: Colors.transparent,
+    borderBottomWidth: 1,
+    height: 20,
+    borderBottomColor: Colors.tabIconDefault,
+  },
+  inValidForm: {
+    borderBottomColor: Colors.buttonRed,
+  },
+  formSwitch: {
+    marginTop: 20,
+  },
+  uploadBotton: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "70%",
+    paddingVertical: 12,
+    borderRadius: 12,
+    backgroundColor: Colors.tintColor,
+    paddingHorizontal: 25,
+  },
+  problemBotton: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "70%",
+    paddingVertical: 12,
+    borderRadius: 12,
+    backgroundColor: Colors.transparent,
+    paddingHorizontal: 25,
+  },
+  createBotton: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "70%",
+    paddingVertical: 12,
+    borderRadius: 12,
+    backgroundColor: Colors.buttonRed,
+    paddingHorizontal: 25,
+    marginBottom: 70,
+  },
+  claimText: {
+    fontSize: 15,
+    fontFamily: "sf-regular",
+    color: "#fff",
+  },
+  problemText: {
+    fontSize: 15,
+    fontFamily: "sf-regular",
+    color: Colors.mainfontColor,
+  },
+});
